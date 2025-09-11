2c24095377b80100d65bd8ef181566e29cab2aec
arif wicaksono septyanto <codelearnid@gmail.com>
2025-09-12 07:03:59 +0800

    update modul 3

---
+++ Script/Week 2/01.object.php
@@ -0,0 +1,16 @@
+<?php
+
+class Produk {
+
+
+}
+
+$televisi = new Produk();
+$mesincuci = new Produk();
+$specker = new Produk();
+
+var_dump($televisi);
+echo "<br>";
+var_dump($mesincuci);
+echo "<br>";
+var_dump($specker);
